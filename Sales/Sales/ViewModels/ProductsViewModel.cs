a595d5dcc3e9e946906aa87db66bce091994e3f7
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2018-08-20 17:53:42 -0500

    MvvmLightLibs and Command

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Common.Models;
+    using GalaSoft.MvvmLight.Command;
     using Sercives;
     using Xamarin.Forms;
 
@@ -11,12 +13,20 @@
     {
         private ApiService apiService;
 
+        private bool isRefreshing;
+
         public ObservableCollection<Product> products;
 
         public ObservableCollection<Product> Products
         {
             get { return this.products; }
             set { this.SetValue(ref this.products, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
         }
 
         public ProductsViewModel()
@@ -27,15 +37,27 @@
 
         private async void LoadProducts()
         {
+            this.IsRefreshing = true;
             var response = await this.apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
             if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
 
             var list = (List<Product>)response.Result;
+            this.IsRefreshing = false;
             this.Products = new ObservableCollection<Product>(list);
+        }
+
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadProducts);
+            }
+
         }
     }
 }
