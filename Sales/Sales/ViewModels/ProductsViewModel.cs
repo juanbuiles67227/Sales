4b9e94fd8906c5e9fee4e2efc27c99d9c449f16a
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2018-08-20 22:22:50 -0500

    Connectivity

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -38,7 +38,17 @@
         private async void LoadProducts()
         {
             this.IsRefreshing = true;
-            var response = await this.apiService.GetList<Product>("https://salesapiservices.azurewebsites.net", "/api", "/Products");
+
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+                return;
+            }
+
+            var url = Application.Current.Resources["UrlAPI"].ToString();
+            var response = await this.apiService.GetList<Product>(url, "/api", "/Products");
             if (!response.IsSuccess)
             {
                 this.IsRefreshing = false;
