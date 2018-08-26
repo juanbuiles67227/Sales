cd60011140f47b957786016443c7472d5f73ed35
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2018-08-26 18:29:51 -0500

    MVC and Bootsrap

--- Sales.Domain/Models/DataContext.cs
+++ Sales.Domain/Models/DataContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sales.Domain.Models
 {
     using System.Data.Entity;
+    using Common.Models;
 
     public class DataContext : DbContext
     {
@@ -9,6 +10,7 @@
 
         }
 
-        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+
     }
 }
